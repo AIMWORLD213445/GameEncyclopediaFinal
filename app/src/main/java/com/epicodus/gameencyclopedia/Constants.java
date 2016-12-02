f0da5f46f834b33c6499a09ccefa7afe0f0062a2
Brian Dutz <bgdutz@gmail.com>
2016-12-02 14:21:59 -0800

    add more precise language for querying the API, start on the Game Model

--- app/src/main/java/com/epicodus/gameencyclopedia/Constants.java
+++ app/src/main/java/com/epicodus/gameencyclopedia/Constants.java
@@ -2,7 +2,7 @@ package com.epicodus.gameencyclopedia;
 
 public class Constants {
     public static final String GIANT_BOMB_KEY = BuildConfig.GIANT_BOMB_KEY;
-    public static final String GIANT_BOMB_URL = "http://www.giantbomb.com/api/search/?";
+    public static final String GIANT_BOMB_URL = "http://www.giantbomb.com/api/search/?resource_type=object&format=json&field_list=name, original_release_date, ";
     public static final String GAME_SEARCH = "query";
     public static final String API_KEY_QUERY_PARAMETER = "api_key";
 }
---
+++ app/src/main/java/com/epicodus/gameencyclopedia/Game.java
@@ -0,0 +1,4 @@
+package com.epicodus.gameencyclopedia;
+
+public class Game {
+}
