6820f625e2a330d59ccf1ce7834c439231431ed0
Brian Dutz <bgdutz@gmail.com>
2016-12-02 15:19:00 -0800

    build out Game class

--- app/src/main/java/com/epicodus/gameencyclopedia/Game.java
+++ app/src/main/java/com/epicodus/gameencyclopedia/Game.java
@@ -1,4 +1,24 @@
 package com.epicodus.gameencyclopedia;
 
 public class Game {
+    private String mName;
+    private String mDeck;
+    private String mImageUrl;
+
+    public Game(String name, String deck, String imageURL) {
+        this mName = name;
+        this mDeck = deck;
+        this mImageUrl = imageURL;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+    public String getDeck() {
+        return mDeck;
+    }
+    public String getmImageUrl() {
+        return mImageUrl;
+
+
 }
