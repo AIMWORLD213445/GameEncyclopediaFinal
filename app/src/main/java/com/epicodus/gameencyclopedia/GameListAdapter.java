99060ad6eff0fefbfa03ab214c699ee2499327c9
Brian Dutz <bgdutz@gmail.com>
2016-12-02 16:55:36 -0800

    continue working on recyclerView and fix bugs/typos

--- app/src/main/java/com/epicodus/gameencyclopedia/GameListAdapter.java
+++ app/src/main/java/com/epicodus/gameencyclopedia/GameListAdapter.java
@@ -2,7 +2,9 @@ package com.epicodus.gameencyclopedia;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,9 +13,6 @@ import java.util.ArrayList;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-/**
- * Created by Guest on 12/2/16.
- */
 public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.GameViewHolder> {
     private ArrayList<Game> mGames = new ArrayList<>();
     private Context mContext;
@@ -22,6 +21,24 @@ public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.GameVi
         mContext =context;
         mGames = games;
     }
+
+    @Override
+    public GameListAdapter.GameViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.game_list_item, parent, false);
+        GameViewHolder viewHolder = new GameViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(GameListAdapter.GameViewHolder holder, int position) {
+        holder.bindGame(mGames.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mGames.size();
+    }
+
 
     public class GameViewHolder extends RecyclerView.ViewHolder {
         @Bind(R.id.gameImageView) ImageView mGameImageView;
