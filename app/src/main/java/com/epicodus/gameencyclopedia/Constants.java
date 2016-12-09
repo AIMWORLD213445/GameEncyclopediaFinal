d5883dc8c04c238e6d8798fed8abfea1ec512ef0
Brian Dutz <bgdutz@gmail.com>
2016-12-09 11:53:08 -0800

    add functionality for saving a specific game to the database

--- app/src/main/java/com/epicodus/gameencyclopedia/Constants.java
+++ app/src/main/java/com/epicodus/gameencyclopedia/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public static final String GAME_SEARCH = "query";
     public static final String API_KEY_QUERY_PARAMETER = "api_key";
     public static final String PREFERENCES_QUERY_KEY = "query";
+    public static final String FIREBASE_CHILD_GAMES = "games";
 }
--- app/src/main/java/com/epicodus/gameencyclopedia/GameDetailFragment.java
+++ app/src/main/java/com/epicodus/gameencyclopedia/GameDetailFragment.java
@@ -6,9 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
@@ -19,10 +23,11 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 
-public class GameDetailFragment extends Fragment {
+public class GameDetailFragment extends Fragment implements View.OnClickListener {
     @Bind(R.id.gameImageView) ImageView mImageView;
     @Bind(R.id.gameNameTextView) TextView mNameView;
     @Bind(R.id.deckTextView) TextView mDescView;
+    @Bind(R.id.saveGameButton) Button mSaveGameButton;
 
     private Game mGame;
 
@@ -54,9 +59,21 @@ public class GameDetailFragment extends Fragment {
         mNameView.setText(mGame.getName());
         mDescView.setText(mGame.getDeck());
 
+        mSaveGameButton.setOnClickListener(this);
+
 
 
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v == mSaveGameButton) {
+            DatabaseReference gameRef = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_GAMES);
+            gameRef.push().setValue(mGame);
+            Toast.makeText(getContext(), "Add to buylist", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
 }
